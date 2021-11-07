3e7338895bf2bc9b6776b474cc942a03d54d6267
Renata Rodrigues <ticare1@hotmail.com>
2021-11-07 11:53:26 -0300

    Implementando o Padrão ViewModel

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WS_Lanches.Repositories;
+using WS_Lanches.ViewModels;
 
 namespace WS_Lanches.Controllers
 {
@@ -21,8 +22,13 @@ namespace WS_Lanches.Controllers
             ViewBag.Lanche = "Lanches";
             ViewData["categoria"] = "Categoria";
 
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+
+            var lancheslistViewModel = new LanchesListViewModel();
+            lancheslistViewModel.Lanches = _lancheRepository.Lanches;
+            lancheslistViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lancheslistViewModel);
         }
     }
 }
---
+++ ViewModels/LanchesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WS_Lanches.Models;
+
+namespace WS_Lanches.ViewModels
+{
+    public class LanchesListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
