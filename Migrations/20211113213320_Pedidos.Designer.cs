1a73fbf514878bca929ae74cea049e12b74f1bc7
Renata Rodrigues <ticare1@hotmail.com>
2021-11-13 18:42:10 -0300

    Expandindo o modelo de dominío - Pedidos

--- Migrations/20211113213320_Pedidos.Designer.cs
+++ Migrations/20211113213320_Pedidos.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WS_Lanches.Context;
 
 namespace WS_Lanches.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211113213320_Pedidos")]
+    partial class Pedidos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
