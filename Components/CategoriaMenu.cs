3f2a1f0dd96006375256ec7122c6f08f297d4809
Renata Rodrigues <ticare1@hotmail.com>
2021-11-13 13:36:05 -0300

    ViewComponent para exibir novos itens de Menu

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WS_Lanches.Repositories;
+
+namespace WS_Lanches.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository) {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke() {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+
+            return View(categorias);
+        }
+    }
+}
