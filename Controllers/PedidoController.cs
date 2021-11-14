ea4f68a36179959c5ed0893130f741b9a90889b5
Renata Rodrigues <ticare1@hotmail.com>
2021-11-14 11:46:18 -0300

    Implementando o PedidoController

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WS_Lanches.Models;
+using WS_Lanches.Repositories;
+
+namespace WS_Lanches.Controllers
+{
+    public class PedidoController : Controller {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra) {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido) {
+            var items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = items;
+
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0) {
+                ModelState.AddModelError("", "Seu carrinho está vazio, inclua um lanche...");
+            }
+
+            if (ModelState.IsValid) {
+                _pedidoRepository.CriarPedido(pedido);
+                _carrinhoCompra.LimparCarrinho();
+                return RedirectToAction("CheckoutCompleto");
+            }
+
+            return View(pedido);
+        }
+
+        public IActionResult CheckoutCompleto() {
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :) ";
+            return View();
+        }
+    }
+}
